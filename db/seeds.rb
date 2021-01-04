a6b1a363b5711632c07473cc5c1d5e1905be6ac4
emmafewer <emmafewer20@gmail.com>
2021-01-04 08:58:12 -0800

    add image to emma seed but need to change image type to be :text

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ User.destroy_all
 Species.destroy_all
 
 
-emma = User.create(name:'emma', username: 'efewer', bio: "hi, I'm emma.", city: 'Glasgow, MT')
+emma = User.create(name:'emma', username: 'efewer', bio: "hi, I'm emma.", city: 'Glasgow, MT', image: "https://lh3.googleusercontent.com/fife/ABSRlIqy_duQvqtSrmKbrKrbU8tvqlgE58UnUnamfETxbgLr8MLTwarTcmmnTokU7LEhX7lLVw8RxnPRCCVm-U24-HOspaL22NiE6InDbNM85EBBwkWnV12pRlrbQfBnRc9fmyW-WIQREvqRvhM-EFy4Z6pH6g_aTZvZM6OcMOBsBXoUzlgYqQZWIODRIQZPk0qaFkZ5Q_L2EeB8gsuDxPiB1tXXkc-fmAwtJZnnnCNcmkaG0gEoz2QEOwYVK1G5D9N-iQIEmsxGrkad-sq1UH3m9g_A9A_Fn231so53h8cD7vAPFd15dj6ZVml4wIvHKEIMPE3JDJz0gYPbw_B84duXkUSYEurIX6z49SD3-4eoGQOyU30m9G_eDoTvTjlkpw3m1cJeF7oVFz7aFykTRhR1fv3GRspaKYCie4rlHL7JbClEoC04CZAh1hVFZ2meP78lZZxoSQlwPx-fI1FQfINgmtXrmygUci-qv_0WrttoC7KhhdyeRjO-vDquSGWKB_NBYKY3V1ptz5N39sWzIdqTm_EIXOvVEaXT4zLkyd_Eg5dGynITWORngyzWXJJwcuLMS-VtCX7dWncDD7RljAWECIM7AnnOjQKKalP4lchK2we_jAk56JaO387ktMVjcCfrYK2Gl-cx4AdlTGusVQgALLz4Jf87lwITFwDEVogClMoIColXspHhSLu9XWsw2doFsfNpTFIDc7821JY=w1164-h873-no?authuser=0")
 dave = User.create(name:'dave', username: 'dwisecar', bio: "hi, I'm dave.", city: 'Seattle, WA')
 adam = User.create(name:'adam')
 emiley = User.create(name:'emiley')
