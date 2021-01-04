78e08905920c1a0144ea6d751e407f9f3845e10f
dwisecar <dwisecar@gmail.com>
2021-01-04 11:38:53 -0800

    Update schema adding user_id to comments table

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,18 @@
 class CommentsController < ApplicationController
+
+    def new
+        @comment = Comment.new
+    end
+
+    def create
+        @comment = Comment.create(comment_params)
+    end
+
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:content, :post_id)
+    end
+
 end
