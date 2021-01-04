4caa273efe4dbec489c5e1c99bae610c526b92e7
emmafewer <emmafewer20@gmail.com>
2021-01-04 10:56:10 -0800

    adding user stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,26 @@
 class User < ApplicationRecord
     has_many :pets, :dependent => :destroy
     has_many :posts, :dependent => :destroy
+
+    def pet_count
+        self.pets.size
+    end
+
+    def species 
+        self.pets.each do |pet|
+            pet.species.name
+        end
+    end
+
+    def species_count
+        counts = Hash.new
+        self.pets.each do |pet|
+            if !counts[pet.species.name]
+                counts[pet.species.name] = 1
+            else
+                counts[pet.species.name] += 1
+            end
+        end
+        counts
+    end
 end
