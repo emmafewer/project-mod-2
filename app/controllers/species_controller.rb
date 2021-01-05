24d8b15c42de7fd1446ca715bc859488844c27ad
emmafewer <emmafewer20@gmail.com>
2021-01-04 17:23:35 -0800

    worked on HTML of pets and users more

--- app/controllers/species_controller.rb
+++ app/controllers/species_controller.rb
@@ -1,2 +1,15 @@
 class SpeciesController < ApplicationController
+    def new
+        @species = Species.new
+    end
+
+    def create
+        @species = Species.create(species_params)
+    end
+
+    private
+
+    def species_params
+        params.require(:species).permit(:name)
+    end
 end
